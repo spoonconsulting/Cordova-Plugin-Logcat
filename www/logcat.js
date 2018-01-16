fbedd97be0071e8106d57101ca09877cbbceca8a
Mevin Dhunnooa <mdhunnooa@gmail.com>
2018-01-16 16:26:57 +0400

    fixed duplicate plugin

--- www/logcat.js
+++ www/logcat.js
@@ -1,11 +1,8 @@
-cordova.define("cordova-plugin-logcat.LogCat", function(require, exports, module) {
-    module.exports = {
+ module.exports = {
         getLogPath: function(successCB, failureCB) {
             cordova.exec(successCB, failureCB, "LogCat", "getLogPath", []);
         },
         logToFile: function(successCB, failureCB) {
             cordova.exec(successCB, failureCB, "LogCat", "logToFile", []);
         },
-    };
-
-});
+ };
